e9002c3e1793e620b8a9b06f5c19f555acb938a0
孙皓亮 <sunhaoliang@Mac-2.local>
2026-05-01 16:55:52 +0800

    index

--- github.sh
+++ github.sh
@@ -2,6 +2,7 @@ python2 jemdoc.py index.jemdoc
 python2 jemdoc.py publications.jemdoc
 python2 jemdoc.py people.jemdoc
 python2 jemdoc.py services.jemdoc
+python2 jemdoc.py courses.jemdoc
 git add .
 git commit -m 'index'
 git push origin main:master
